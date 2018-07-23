3b93491b3527ede2ad576524185254ddc33222a2
Cody Nicoll <cdnicoll@gmail.com>
2018-07-22 19:01:47 -0700

    Added array examples

--- hash_array_looping.rb
+++ hash_array_looping.rb
@@ -87,3 +87,49 @@ end
 FOO.each do |key|
   puts "key: #{key}"
 end
+
+
+puts "------------------------------------------------"
+foo = ["foo", "bar", "nax", "pax"]
+puts foo[1]
+
+puts foo.include?("jar")
+puts foo.include?("foo")
+
+puts ""
+puts "Calling reverse will not mutate the array"
+p foo.reverse
+p foo
+puts "However using a bang(!) will mutate the array"
+p foo.reverse!
+p foo
+
+puts ""
+# To convert to an array
+p (1..15).to_a
+
+puts ""
+# random array of 0-100
+y = (0..10).to_a.shuffle
+p y
+x = (0..10).to_a.shuffle!
+p x
+
+puts ""
+puts "Append to array"
+x = (1..10).to_a
+x << 15
+p x
+x.push(20)
+p x
+x.unshift(-1)
+p x
+
+puts ""
+for number in x
+  puts number
+end
+
+puts ""
+puts "using select works by giving it a boolean expression"
+p x.select { |num| num.odd? }
