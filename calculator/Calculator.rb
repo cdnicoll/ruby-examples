d0fae169d0e5510409bee9d592ac5fa28b5d48b7
cdnicoll <cdnicoll@gmail.com>
2019-01-11 14:32:07 -0800

    Added Calculator class

---
+++ calculator/Calculator.rb
@@ -0,0 +1,26 @@
+class Calculator
+    def plus(n1, n2)
+        n1 + n2
+      end
+    
+      def minus(n1, n2)
+        n1 - n2
+      end
+    
+      def multiply(n1, n2)
+        n1 * n2
+      end
+    
+      def divide(n1, n2)
+        n1 / other
+      end
+end
+    
+# $ irb
+# > class Calculator
+# > end
+# > calculator = Calculator.new
+# > calculator.class
+# => Calculator
+# > calculator.is_a?(Calculator)
+# => true
---
+++ calculator/main.rb
@@ -0,0 +1,5 @@
+# ruby main.rb
+require './Calculator'
+
+cal = Calculator.new
+puts cal.plus(2, 3)
