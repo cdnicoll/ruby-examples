748d94984c14400bf8b8e03a2a96c81220851892
cdnicoll <cdnicoll@gmail.com>
2019-01-11 14:33:53 -0800

    Added Calculator class

--- calculator/Calculator.rb
+++ calculator/Calculator.rb
@@ -12,7 +12,7 @@ class Calculator
       end
     
       def divide(n1, n2)
-        n1 / other
+        n1 / n2
       end
 end
     
--- calculator/main.rb
+++ calculator/main.rb
@@ -3,3 +3,6 @@ require './Calculator'
 
 cal = Calculator.new
 puts cal.plus(2, 3)
+puts cal.minus(2, 3)
+puts cal.multiply(2, 3)
+puts cal.divide(2, 3)
